e883b05c42db83ebebea36b870a201b022f2aa53
maehara08 <aso.riiiku08@gmail.com>
2017-02-08 18:23:38 +0900

    search fragment

--- app/src/main/kotlin/com/riku/land/cs/githubsercher/BaseApplication.kt
+++ app/src/main/kotlin/com/riku/land/cs/githubsercher/BaseApplication.kt
@@ -1,7 +1,13 @@
 package com.riku.land.cs.githubsercher
 
+import android.support.annotation.IdRes
 import android.support.multidex.MultiDexApplication
+import android.view.View
 
 open class BaseApplication : MultiDexApplication() {
 
 }
+
+//Extension
+fun <T : View> View.bindView(@IdRes resId: Int): T =
+        this.findViewById(resId) as T
--- app/src/main/kotlin/com/riku/land/cs/githubsercher/ui/scene/search/SearchFragment.kt
+++ app/src/main/kotlin/com/riku/land/cs/githubsercher/ui/scene/search/SearchFragment.kt
@@ -1,18 +1,37 @@
 package com.riku.land.cs.githubsercher.ui.scene.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.riku.land.cs.githubsercher.BaseFragment
 import com.riku.land.cs.githubsercher.R
+import com.riku.land.cs.githubsercher.bindView
+import kotlin.properties.Delegates
 
 /**
  * Created by riku_maehara on 17/02/08.
  */
 
 public class SearchFragment : BaseFragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
-            inflater.inflate(R.layout.fragment_search, container, false)
+    companion object{
+        const val TAG = "SearchFragment"
+    }
 
+
+    private var searchDebugButton : Button by Delegates.notNull()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+            inflater.inflate(R.layout.fragment_search, container, false).apply {
+                searchDebugButton = bindView(R.id.debugButton)
+            }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        searchDebugButton.setOnClickListener({
+            Log.d(TAG, "onClicked")
+        })
+    }
 }
